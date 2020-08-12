1b733f724800cc4a73cf9d6e0abc8ae58876fc09
Matt Carver <matt@edfleba.net>
2020-08-12 15:15:59 +0100

    fixing manifest

--- site/scripts/main.js
+++ site/scripts/main.js
@@ -432,7 +432,6 @@ function stopSound(name) {
 }
 
 function setupSounds() {
-  console.log(Memory.exists('standup_mute'), typeof Memory.getBoolean('standup_mute'), Memory.getBoolean('standup_mute'));
   if (Memory.exists('standup_mute')) {
     muted = Memory.getBoolean('standup_mute');
   } else {
